c05e8a20e2ee6a99a008f91b86fd4792d1ddb8d1
oscar <oscardiazmaldonado93@gmail.com>
2023-02-27 00:10:06 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_4/Views/VerImagen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio1_4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerImagen : ContentPage
+    {
+        public VerImagen()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
